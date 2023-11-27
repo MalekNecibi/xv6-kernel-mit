af000941c50543731680b994bb9f462630ec7628
Malek N <Malek@Necibi.us>
2023-11-27 13:57:03 -0600

    primes : all tests pass

--- user/primes.c
+++ user/primes.c
@@ -3,94 +3,58 @@
 #include "user/user.h"
 
 #define MAX_PIPELINE 35
-#define NUM_CHARS_IN_INT sizeof(int) / sizeof(char)
+enum pipe_ops{ Read, Write };
 
-union Int_Char_Array {
-    int integer;
-    char chars[NUM_CHARS_IN_INT];
-};
+int main(int argc, char *argv[]) {
+	int parent;
+    int child[2];
 
-enum pipe_sides{Read, Write};
-
-void swap(int *p0, int *p1);
-
-int main(int argc, char* argv[]) {
-    int parent[2];  // [ read, write ]
-    int child[2];   // [ read, write ]
-    
-    union Int_Char_Array num;
-
-    
-    pipe(parent);
     pipe(child);
 
-    
-    close(parent[Read]);
-    
-    // Seed to Pipeline
-    for (int i = 2; i <= MAX_PIPELINE; i++) {
-        num.integer = i;
-        write(child[Write], num.chars, sizeof(int));
-    }
-    close(child[Write]);    // no need to write from seed again
-    
-    swap(parent, child);
+    // Process: Seed to Pipeline
+	if (0 == fork()) {
+		for (int i = 2; i <= MAX_PIPELINE; i++) {
+			write(child[Write], &i, sizeof(int));
+		}
+		close(child[Write]);
 
-    
-    int pid;
-    // Pipeline
-    while (0 != read(parent[Read], num.chars, sizeof(int))) {
-        int prime = num.integer;
-        printf("%d: prime %d\n", getpid(), prime);
+		exit(0);
+	}
+
+	close( child[Write] );
+	parent = child[Read];
+	
+
+	int prime; 
+    while (read(parent, &prime, sizeof(int))) {
+		printf("prime %d\n", prime); 
+		
+		int child[2];
+		int candidate;
+
+		pipe(child);
+//        printf("new pipe : %d,%d\n", child[Read], child[Write]);
         
-        pipe(child);
-         
-        pid = fork();
-        if (pid > 0) {
-            // Parent Process
-            
-            while(0 != read(parent[Read], num.chars, sizeof(int))) {
-                if (0 != num.integer % prime) {
-                    write(child[Write], num.chars, sizeof(int));
-                }
-            }
-            
-            close(child[Write]);
-            close(parent[Read]);
-                
-            printf("%d*", getpid());
-            wait((int *) 0);
-            printf("|");
-            exit(0);
+        // Process: Syphon out invalid candidates
+		if (0 == fork()) {
+			while (read(parent, &candidate, sizeof(int))) {
+				if (0 != candidate % prime) {
+					write(child[Write], &candidate, sizeof(int));
+				}
+			}
 
-            
+			close(child[Write]);
+			close(parent);
+			exit(0);
+		}
         
-        } else if (0 == pid) {
-            // Spawned next process in pipeline
-            swap(parent, child);
-            continue;
-        }
+        // process complete, prep for next in pipeline
+		close(parent);
+		close(child[Write]);
+		parent = child[Read];
+	
     }
-    
-    //close(parent[Read]);
-
-    printf("done\n");
-                
-
-    exit(0);
+    wait((int *) 0);
+	exit(0);
 }
 
-void swap(int *p0, int *p1) {
-    int a, b;
-
-    a = p0[0];
-    b = p0[1];
-
-    p0[0] = p1[0];
-    p0[1] = p1[1];
-
-    p1[0] = a;
-    p1[1] = b;
-    
-    return;
-}
